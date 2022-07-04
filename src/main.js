c2b30223054359e0a0661535bb977b1b732c9878
pxmaciej <56204020+pxmaciej@users.noreply.github.com>
2022-07-04 22:28:40 +0200

    VueRouter

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import Routes from './routes'
 
 Vue.config.productionTip = false
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  routes: Routes
+})
 
 new Vue({
+  router: router,
   render: h => h(App),
 }).$mount('#app')
+
---
+++ src/routes.js
@@ -0,0 +1,12 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+Vue.use(VueRouter);
+
+export const routes = [
+
+    {
+        path : '/login',
+        
+    },
+];

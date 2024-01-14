26eb59c9b1478c20692b12cf2e0065edfe5704f5
pxmaciej <56204020+pxmaciej@users.noreply.github.com>
2024-01-14 19:35:39 +0100

    Correct hook on profile data

--- src/Profile.vue
+++ src/Profile.vue
@@ -2,16 +2,17 @@
   <v-container>
     <v-row>
       <div class="col-6">
-        <formProfile></formProfile>
+        <formProfile @submit="getProfile"></formProfile>
       </div>
       <div class="col-6">
-        <profileData></profileData>
+        <profileData :profile="profile"></profileData>
       </div>
     </v-row>
   </v-container>
 </template>
 
 <script>
+import axios from "axios";
 import formProfile from "./components/formProfile";
 import profileData from "@/components/profileData";
 import AuthService from '@/services/AuthService'
@@ -23,13 +24,28 @@ export default {
     formProfile,
     profileData
   },
+
+  data() {
+    return {
+      profile: [],
+    }
+  },
   
   mounted: async function () {
-    await AuthService.isAuthenticated(this)
+    if (await AuthService.isAuthenticated(this)) {
+      this.getProfile();
+    }
+  },
+
+  methods: {
+    getProfile() {
+      axios.get(
+      this.$root.API_AUTH + 'profile',
+      {headers: {"Authorization": 'Bearer ' + this.$store.state.token}}
+    ).then(res => {
+      this.profile = res.data;
+    });
+    }
   }
 }
 </script>
-
-<style scoped>
-
-</style>
--- src/components/profileData.vue
+++ src/components/profileData.vue
@@ -9,7 +9,7 @@
               <v-icon v-text="icons.mdiAccount"></v-icon>
             </v-list-item-icon>
             <v-list-item-title>Imie:</v-list-item-title>
-            <v-list-item-subtitle>{{ name }}</v-list-item-subtitle>
+            <v-list-item-subtitle>{{ profile.name }}</v-list-item-subtitle>
           
           </v-list-item>
         </v-list-item>
@@ -19,7 +19,7 @@
               <v-icon v-text="icons.mdiLoginVariant"></v-icon>
             </v-list-item-icon>
             <v-list-item-title>Login:</v-list-item-title>
-            <v-list-item-subtitle>{{ login }}</v-list-item-subtitle>
+            <v-list-item-subtitle>{{ profile.login }}</v-list-item-subtitle>
           </v-list-item>
         </v-list-item>
         <v-list-item>
@@ -28,7 +28,7 @@
               <v-icon v-text="icons.mdiPhone"></v-icon>
             </v-list-item-icon>
             <v-list-item-title>Telefon:</v-list-item-title>
-            <v-list-item-subtitle>{{ phone }}</v-list-item-subtitle>
+            <v-list-item-subtitle>{{ profile.phone }}</v-list-item-subtitle>
           </v-list-item>
         </v-list-item>
       </v-list>
@@ -37,35 +37,20 @@
 </template>
 
 <script>
-
-import axios from "axios";
 import {mdiAccount, mdiLoginVariant, mdiPhone} from "@mdi/js";
 
 export default {
   name: "profileData",
-  
+  props: ['profile'],
+
   data() {
     return {
-      name: '',
-      login: '',
-      phone: '',
       icons: {
         mdiAccount,
         mdiLoginVariant,
         mdiPhone
       }
     }
-  },
-  
-  created() {
-    axios.get(
-      this.$root.API_AUTH + 'profile',
-      {headers: {"Authorization": 'Bearer ' + this.$store.state.token}}
-    ).then(res => {
-      this.name = res.data.name;
-      this.login = res.data.login;
-      this.phone = res.data.phone;
-    });
   }
 }
 </script>

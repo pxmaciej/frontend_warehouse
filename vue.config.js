51027d2d8df980c48ae881470369c0bc77cf03b7
pxmaciej <56204020+pxmaciej@users.noreply.github.com>
2024-01-28 20:32:49 +0100

    CROS

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,14 @@
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'https://magazyn.000.pe',
+        changeOrigin: true,
+        pathRewrite: { '^/api': '/api' }
+      }
+    }
+  }
 }
